4673aacfb300772b43938041e13ae67c632cbd8d
shoter <lubiebardzozelki@gmail.com>
2018-03-19 06:14:03 +0100

    Added UnitOfWork

--- Infrastructure/IUnitOfWork.cs
+++ Infrastructure/IUnitOfWork.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Infrastructure.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,6 @@ namespace Infrastructure
 {
     public interface IUnitOfWork
     {
-        
+        IBannerRepository BannerRepository { get; }
     }
 }
---
+++ Mongo/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infrastructure.Repositories;
+using MongoDB.Driver;
+using Mongo.Repositories;
+
+namespace Mongo
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public IBannerRepository BannerRepository { get; private set; }
+
+        public UnitOfWork(IMongoDatabase mongoDatabase)
+        {
+            BannerRepository = new BannerRepository(mongoDatabase);
+        }
+
+    }
+}
